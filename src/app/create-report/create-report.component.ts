4af139a5e08c9d1e4d8ffe353e5edcd84c90d46b
Prerna Bhandari <prernabhandari00@gmail.com>
2023-05-09 10:13:37 +0530

    CHANGES

--- src/app/create-report/create-report.component.ts
+++ src/app/create-report/create-report.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as Papa from 'papaparse';
-import * as fs from 'fs-extra';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 
@@ -44,19 +43,25 @@ export class CreateReportComponent implements OnInit {
     const studentDetailsInput: HTMLInputElement = <HTMLInputElement>document.getElementById('studentDetailsFile');
     const studentResponsesInput: HTMLInputElement = <HTMLInputElement>document.getElementById('studentResponsesFile');
     const answerKeyInput: HTMLInputElement = <HTMLInputElement>document.getElementById('answerKeyFile');
-  
+
     // Get report name
     const reportName: string = reportNameInput.value;
-  
+
     // Get student details file
     const studentDetailsFile: File | null = studentDetailsInput.files?.[0] ?? null;
-  
+
     // Get student responses file
     const studentResponsesFile: File | null = studentResponsesInput.files?.[0] ?? null;
-  
+
     // Get answer key file
     const answerKeyFile: File | null = answerKeyInput.files?.[0] ?? null;
-  
+
+    // Check if all required fields have been filled out
+    if (!reportName || !studentDetailsFile || !studentResponsesFile || !answerKeyFile) {
+        alert("Please fill out all required fields before submitting the form.");
+        return;
+    }
+
     // Parse file contents to JSON using Papa Parse
     const parseFile = (file: File | null): Promise<any[]> => {
         return new Promise((resolve, reject) => {
@@ -116,6 +121,7 @@ export class CreateReportComponent implements OnInit {
     });
 }
 
+
   
 
   
@@ -129,3 +135,30 @@ export class CreateReportComponent implements OnInit {
   
 
 }
+
+
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   user: 'your_username',
+//   host: 'your_host',
+//   database: 'your_database_name',
+//   password: 'your_password',
+//   port: 5432, // the default port for PostgreSQL
+// });
+
+// await client.connect();
+
+// const answerKey = [{...}, {...}, {...}, ...];
+// const reportName = 'q';
+// const studentDetails = [{...}, {...}, {...}, ...];
+// const studentResponses = [{...}, {...}, {...}, ...];
+
+// const query = 'INSERT INTO your_table_name (answer_key, report_name, student_details, student_responses) VALUES ($1, $2, $3, $4)';
+
+// const values = [answerKey, reportName, studentDetails, studentResponses];
+
+// await client.query(query, values);
+
+// await client.end();
