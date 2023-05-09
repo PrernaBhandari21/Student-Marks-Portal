b576f48fc7ac81a0058bb70216e8ba75f924dec3
Prerna Bhandari <prernabhandari00@gmail.com>
2023-05-09 13:52:54 +0530

    saving to api

--- src/app/server.js
+++ src/app/server.js
@@ -6,19 +6,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// An array to store the submitted data
+let submittedData = [];
+
 app.post('/api/reportData', (req, res) => {
     const data = req.body;
-    console.log("dataa received is :",data);
-    res.json({ message: 'Data received!',
-data : data });
+    console.log("data received is:", data);
+    
+    // Add the submitted data to the array
+    submittedData.push(data);
+
+    // Send back the submitted data as a response
+    res.json({ message: 'Data received!', data: data });
 });
 
-// Add a route handler for the root URL
-app.get('/API', (req, res) => {
-    res.send('Welcome to my API aserver!');
-  });
+// Route handler to display all the submitted data
+app.get('/api/reportData', (req, res) => {
+    // Send back the array of submitted data as a response
+    res.json(submittedData);
+});
 
-  
+// Route handler for the root URL
+app.get('/api', (req, res) => {
+    res.send('Welcome to my API server!');
+});
+
 const PORT = process.env.PORT || 4200;
 
 app.listen(PORT, () => {
